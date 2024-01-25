066de365829e34f7b665e523720f2ceb0580305f
Bhekisile <bhekisilejozi@gmail.com>
2024-01-26 00:58:38 +0200

    Add seven days forecast on the bottom part of ui

--- src/component/Forecast/Forecast.js
+++ src/component/Forecast/Forecast.js
@@ -4,22 +4,69 @@ import {
   AccordionItemButton,
   AccordionItemHeading,
 } from 'react-accessible-accordion';
+import PropTypes from 'prop-types';
+
+const weekDays = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
 
 function Forecast({ data }) {
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = weekDays
+    .slice(dayInAWeek, weekDays.length)
+    .concat(weekDays.slice(0, dayInAWeek));
+
   return (
-    <div>
-      <label className="title">Daily</label>
+    <>
+      <label htmlFor="title" className="title">
+        Daily
+        <input type="text" id="title" />
+      </label>
       <Accordion allowZeroExpanded>
-        {data.list.splice(0, 7).map((item, idx) => {
-          <AccordionItem key={idx}>
+        {data.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={item.dt}>
             <AccordionItemHeading>
-              <AccordionItemButton>Hello</AccordionItemButton>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="weather"
+                    className="icon-small"
+                    src={`icons/${item.weather[0].icon}.png`}
+                  />
+                  <label htmlFor="day" className="day">{ forecastDays[idx] }</label>
+                  <label htmlFor="description" className="description">{ item.weather[0].description }</label>
+                  <label htmlFor="minMax" className="min-max">
+                    { Math.round(item.main.temp_min) }
+                    °C /
+                    {' '}
+                    { Math.round(item.main.temp_max) }
+                    °C
+                  </label>
+                </div>
+              </AccordionItemButton>
             </AccordionItemHeading>
-          </AccordionItem>;
-        })}
+          </AccordionItem>
+        ))}
       </Accordion>
-    </div>
+    </>
   );
 }
+
+Forecast.propTypes = {
+  data: PropTypes.shape({
+    weather: PropTypes.string.isRequired,
+    list: PropTypes.objectOf(PropTypes.shape({
+      main: PropTypes.arrayOf(PropTypes.shape({
+        temp: PropTypes.number.isRequired,
+      })),
+    })),
+  }).isRequired,
+};
 
 export default Forecast;
