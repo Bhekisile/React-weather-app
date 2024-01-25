10f3a293ef9a5bca84e0302f078bb19e5c86e277
Bhekisile <bhekisilejozi@gmail.com>
2024-01-25 10:18:05 +0200

    Add weather icons and top part of current weather

--- src/component/CurrentWeather/CurrentWeather.js
+++ src/component/CurrentWeather/CurrentWeather.js
@@ -4,10 +4,12 @@ function CurrentWeather() {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">Johannesburg</p>
-        <p className="weather-description">Sunny</p>
+        <div>
+          <p className="city">Johannesburg</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
-      <img alt="weather" className="weather-icon" src="icons/01d.png" />
     </div>
   );
 }
